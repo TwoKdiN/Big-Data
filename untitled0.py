8248c41b01845c22ea343443b74dbf07bc72137f
TwoKdiN <kostas.katsiris@gmail.com>
2024-07-12 13:58:42 +0300

    Change Parameteres

--- untitled0.py
+++ untitled0.py
@@ -19,23 +19,23 @@ random.seed(seed)
 #Part1 
 W.addNode('Start0', 1, 0)
 W.addNode('Start1', 2, 0)
-W.addLink('Link0', 'Start0', 'Start1', length=500, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('Link0', 'Start0', 'Start1', length=500, free_flow_speed=50, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
 
 #Part2
 W.addNode('LStreet1', 3, 2)
 W.addNode('LStreet2', 3, 1)
 W.addNode('LStreet3', 3, 0)
-W.addLink('LGroup1', 'Start1', 'LStreet1', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
-W.addLink('LGroup2', 'Start1', 'LStreet2', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
-W.addLink('LGroup3', 'Start1', 'LStreet3', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('LGroup1', 'Start1', 'LStreet1', length=1000, free_flow_speed=50, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('LGroup2', 'Start1', 'LStreet2', length=1000, free_flow_speed=50, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('LGroup3', 'Start1', 'LStreet3', length=1000, free_flow_speed=50, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
 
 #Part3
 W.addNode('RStreet1', 6, 2)
 W.addNode('RStreet2', 6, 1)
 W.addNode('RStreet3', 6, 0)
-W.addLink('Street1', 'LStreet1','RStreet1', length=1000, free_flow_speed=10, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
-W.addLink('Street2', 'LStreet2','RStreet2', length=1000, free_flow_speed=10, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
-W.addLink('Street3', 'LStreet3','RStreet3', length=1000, free_flow_speed=10, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('Street1', 'LStreet1','RStreet1', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('Street2', 'LStreet2','RStreet2', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
+W.addLink('Street3', 'LStreet3','RStreet3', length=1000, free_flow_speed=20, number_of_lanes = 1, jam_density=0.2, merge_priority=0.5)
 
 #Part4
 W.addNode('Dest1', 7,0)
@@ -60,9 +60,9 @@ W.exec_simulation()
 
 W.analyzer.print_simple_stats()
 
-W.show_network()
+#W.show_network()
 
-# #Create GIF
+#Create GIF
 # W.analyzer.network_fancy(animation_speed_inverse=15, sample_ratio=0.3, interval=3, trace_length=5, network_font_size=1)
 
 # with open("out/anim_network_fancy.gif", "rb") as f:
